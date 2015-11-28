aa04b7d41a0b670131ed0f14fc475cedbdd9d7da
paulmotey <moteyalpha@gmail.com>
2015-11-28 02:21:40 -0600

    Update changes fixed

--- FIXME.c
+++ FIXME.c
@@ -1,5 +1,8 @@
 PROBLEMS and solutions
-This really isn't C at all, but I like the fact that it highlights FIXME in the comments //FIXME
+This really isn't C at all, but I like the fact that it highlights FIXME in the comments with my IDE//FIXME
 //FIXME
-1. contact does not work is 500
-2. upload does not work is 500
+1. contact does not work is 500 FIXED
+2. upload does not work is 500 FIXED
+3. Odd way that CSS works with file upload works differently in Kong and Firefox and button is located poorly
+	something from the standard handlebars library that is odd .
+	
